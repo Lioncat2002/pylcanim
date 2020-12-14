75fb8fcbfbc94b4caf2de7e70a7f70c7402208da
lioncatdev <sankhayan2002@gmail.com>
2020-12-14 17:53:51 +0530

    Added support for multidimensional spritesheets

--- pylcanimtest.py
+++ pylcanimtest.py
@@ -7,6 +7,7 @@ c=0
 data={}
 img=0
 l=[]
+
 width,height=0,0
 def init(sfile):
     global data,img,width,height,l
@@ -17,7 +18,7 @@ def init(sfile):
     height = data['textureAtlas']['regionHeight']
     for key in data['cycles'].keys():
         l.append(key)
-    
+
     img=pygame.image.load(data['textureAtlas']['texture'])
 
 def lcAnim(fpslimit=2,animpos=0):
@@ -27,8 +28,11 @@ def lcAnim(fpslimit=2,animpos=0):
     if c>=len(data['cycles'][l[animpos]]['frames']):
         c=0
     if c<len(data['cycles'][l[animpos]]['frames']):
-        img.set_clip(pygame.Rect(data['cycles'][l[animpos]]['frames'][c-1]*width,animpos*height,width,height))  # Locate the sprite you want
+    
+        #img.set_clip(pygame.Rect(data['cycles'][l[animpos]]['frames'][c-1]*width,animpos*height,width,height))  # Locate the sprite you want
+        img.set_clip(pygame.Rect((c-1)*width,animpos*height,width,height))  # Locate the sprite you want
         draw_me = img.subsurface(img.get_clip())  # Extract the sprite you want
+       
         if(fps>fpslimit):
             c =c+1
             fps=0
